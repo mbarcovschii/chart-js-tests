43d9965b444bb97048788fc927d7abe15cd4fda0
mihail.barcovschii <mihail.barcoschii@enabld.tech>
2023-01-13 09:04:57 +0200

    Added changeLayout button to bar chart page

--- src/app/chart-components/bar-chart/bar-chart.component.ts
+++ src/app/chart-components/bar-chart/bar-chart.component.ts
@@ -27,14 +27,36 @@ export class BarChartComponent implements OnInit {
     this.barChartData = {
       labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
       datasets: [
-        { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-        { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+        { data: [65, -59, 80, -81, 56, -55, 40], label: 'Series A' },
+        { data: [28, -48, 40, 19, -86, 27, 90], label: 'Series B' }
       ]
     };
     this.barChartOptions = {
-      responsive: true
+      responsive: true,
+      indexAxis: 'y',
+      elements: {
+        bar: {
+          borderWidth: 2,
+        }
+      },
+      plugins: {
+        legend: {
+          position: 'right',
+        },
+        title: {
+          display: true,
+          text: 'Bar Chart Example'
+        }
+      }
     };
     this.barChartLegend = true;
     this.chart?.render();
+  }
+
+  public changeLayout(): void {
+    if (this.chart?.options?.indexAxis) {
+      this.chart.options.indexAxis = this.chart.options.indexAxis == 'x' ? 'y' : 'x';
+      this.chart?.render();
+    }
   }
 }
